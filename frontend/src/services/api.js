aa37a8f3c874e109d9d4827cf335fb02c8f0767e
Sanjib70 <sanjibsen234@gmail.com>
2026-04-07 00:53:08 +0530

    Updated API URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,7 +4,7 @@ import axios from 'axios';
  * Axios instance for Forest Guardian API
  */
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // backend base URL
+  baseURL: '/api', // backend base URL
   headers: {
     'Content-Type': 'application/json',
   },
